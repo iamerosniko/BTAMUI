b55d723a417a1eaa7ec4dd4e962638355db24180
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2017-10-17 08:52:39 +0800

    up

--- src/app/mod-maintenance/maintenance.routing.ts
+++ src/app/mod-maintenance/maintenance.routing.ts
@@ -2,9 +2,23 @@ import { NgModule,OnInit } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MaintenanceComponent } from './maintenance/maintenance.component';
+import { MaintenanceAppComponent } from './maintenance/maintenance-app/maintenance-app.component';
+import { MaintenanceTablesComponent } from './maintenance/maintenance-tables/maintenance-tables.component';
+import { MaintenanceGroupsComponent } from './maintenance/maintenance-groups/maintenance-groups.component';
+import { MaintenanceModulesComponent } from './maintenance/maintenance-modules/maintenance-modules.component';
+import { MaintenanceUsersComponent } from './maintenance/maintenance-users/maintenance-users.component';
+
 const appRoutes: Routes = [
     
-    { path: 'Maintenance', component: MaintenanceComponent }
+    { path: 'Maintenance', component: MaintenanceComponent ,children :[
+        { path: '', redirectTo: 'Apps', pathMatch: 'full' },
+        { path: 'Apps', component:MaintenanceAppComponent },
+        { path: 'Tables', component:MaintenanceTablesComponent },
+        { path: 'Groups', component:MaintenanceGroupsComponent },
+        { path: 'Modules', component:MaintenanceModulesComponent },
+        { path: 'Users', component:MaintenanceUsersComponent }
+        
+    ]}
 ];
 
 @NgModule ({
